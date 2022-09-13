762b8087b57058a56a51b03a7c23a51081d14513
CoolRy <that007guy@hotmail.com>
2022-09-13 11:56:31 -0400

    create game model and add to profile model

--- models/profile.js
+++ models/profile.js
@@ -1,13 +1,20 @@
 import mongoose from 'mongoose'
+const Schema = mongoose.Schema
 
-const profileSchema = new mongoose.Schema({
-  email: { type: String, required: true, lowercase: true, unique: true },
+const profileSchema = new Schema({
   name: String,
-  photo: { type: String }
-},{
-  timestamps: true,
+  avatar: String,
+  xboxTag: String,
+  steamTag: String,
+  playstationTag: String,
+  friends: [{type: Schema.Types.ObjectId, ref:'Profile'}],
+  gameCollection: [{type: Schema.Types.ObjectId, ref:'Game'}],
+}, {
+  timestamps: true
 })
 
 const Profile = mongoose.model('Profile', profileSchema)
 
-export { Profile }
+export {
+  Profile
+}
