edcfbc691e7a84731242e522d520453e3aee86da
CoolRy <that007guy@hotmail.com>
2022-09-15 12:54:55 -0400

    stub up controller,routes and mount routes

---
+++ controllers/games.js
@@ -0,0 +1,5 @@
+import { Game } from "../models/game.js";
+
+export {
+  
+}
---
+++ routes/games.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import * as gamesCtrl from '../controllers/games.js'
+// import { decodeFromToken, checkAuth } from '../middleware/auth.js'
+
+const router = Router()
+
+/*---------- Public Routes ----------*/
+
+/*---------- Protected Routes ----------*/
+
+export {
+  router
+}
