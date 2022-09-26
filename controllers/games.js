c600aca7c2a64a07fc7c01a6d080e4f367e0eb65
CoolRy <that007guy@hotmail.com>
2022-09-26 15:47:32 -0400

    back end api stub up

--- controllers/games.js
+++ controllers/games.js
@@ -1,5 +1,20 @@
-import { Game } from "../models/game.js";
+import { Game } from '../models/game.js'
+import axios from 'axios'
+
+function search(req, res) {
+  axios.get(`https://api.rawg.io/api/games?page_size=10&search=${req.body.search}&key=${process.env.API_KEY}`)
+  .then((response) => {
+    res.render("games/results", {
+      title: "Search Results",
+      results: response.data.results,
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
 
 export {
-  
+  search
 }
